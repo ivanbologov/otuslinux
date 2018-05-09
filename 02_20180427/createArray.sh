d1e1615cdb9367ecd2fe91709c15d505f145c184
ivanbologov <ivan.bologov@gmail.com>
2018-05-09 14:42:23 +0300

    fixed creation of array deleted useless creation of gpt label for mdadm

--- 02_20180427/createArray.sh
+++ 02_20180427/createArray.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 # creating RAID50 on empty disks
-# creating gpt labels for the first raid 5
-parted -a optimal -s /dev/sdb mklabel gpt
-parted -a optimal -s /dev/sdc mklabel gpt
-parted -a optimal -s /dev/sdd mklabel gpt
-# creating gpt labels for the second raid 5
-parted -a optimal -s /dev/sde mklabel gpt
-parted -a optimal -s /dev/sdf mklabel gpt
-parted -a optimal -s /dev/sdg mklabel gpt
-# killing partitions on disks
-dd if=/dev/zero bs=512 count=1 of=/dev/sdb
-dd if=/dev/zero bs=512 count=1 of=/dev/sdc
-dd if=/dev/zero bs=512 count=1 of=/dev/sdd
-dd if=/dev/zero bs=512 count=1 of=/dev/sde
-dd if=/dev/zero bs=512 count=1 of=/dev/sdf
-dd if=/dev/zero bs=512 count=1 of=/dev/sdg
 # creating RAID5 arrays
 mdadm --create /dev/md0 -l 5 -n 3 /dev/sdb /dev/sdc /dev/sdd
 mdadm --create /dev/md1 -l 5 -n 3 /dev/sde /dev/sdf /dev/sdg
 # creating RAID0 over RAID5 arrays
 mdadm --create /dev/md2 -l 0 -n 2 /dev/md0 /dev/md1
-# add fs to RAID50
-mkfs.ext4 /dev/md2
-# creating config
+# creating config 
 mkdir /etc/mdadm
 echo "DEVICE partitions" > /etc/mdadm/mdadm.conf
 mdadm --detail --scan --verbose | awk '/ARRAY/ {print}' >> /etc/mdadm/mdadm.conf
-echo "$(blkid | grep /dev/md2 | awk '{print $2}') /mnt ext4 defaults 0 0" >> /etc/fstab
-mount /mnt
-#
+# creating gpt label and adding partitions
+parted -a optimal -s /dev/md2 mklabel gpt mkpart test1 ext4 0% 10% mkpart test2 ext4 10% 20% mkpart test3 ext4 20% 30% mkpart test4 ext4 30% 40% mkpart test5 ext4 40% 100%
+# formatting partitions 
+mkfs.ext4 /dev/md2p1
+mkfs.ext4 /dev/md2p2
+mkfs.ext4 /dev/md2p3
+mkfs.ext4 /dev/md2p4
+mkfs.ext4 /dev/md2p5
+# adding mount points
+mkdir /mnt/test{1,2,3,4,5}
+echo "$(blkid | grep /dev/md2p1 | awk '{print $2}') /mnt/test1 ext4 defaults 0 0" >> /etc/fstab
+echo "$(blkid | grep /dev/md2p2 | awk '{print $2}') /mnt/test2 ext4 defaults 0 0" >> /etc/fstab
+echo "$(blkid | grep /dev/md2p3 | awk '{print $2}') /mnt/test3 ext4 defaults 0 0" >> /etc/fstab
+echo "$(blkid | grep /dev/md2p4 | awk '{print $2}') /mnt/test4 ext4 defaults 0 0" >> /etc/fstab
+echo "$(blkid | grep /dev/md2p5 | awk '{print $2}') /mnt/test5 ext4 defaults 0 0" >> /etc/fstab
+mount /mnt/test1
+mount /mnt/test2
+mount /mnt/test3
+mount /mnt/test4
+mount /mnt/test5
