0db883ea35c0037753516a099f499fe43e04bae0
ivanbologov <ivan.bologov@gmail.com>
2019-02-22 00:27:29 +0300

    Добавил дз 23 Сценарии iptables

---
+++ 201810/23 Iptables scenarios/knock.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# call example ./knock.sh vagrant 192.168.255.1 2222 3333 4444
+
+USER=$1
+shift
+HOST=$1
+shift
+for ARG in "$@"
+do
+    # scan with fin flag to escape tcp retrasmission when using syn only
+    nmap -sF --max-retries 0 -p $ARG $HOST
+    # for udp knock
+    #nmap -sU -Pn --max-retries 0 -p U:$ARG $HOST
+done
+sleep 1
+ssh $USER@$HOST
